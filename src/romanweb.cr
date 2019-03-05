630c0b4238a1b4a32dc7e0efe7130bf580463f9c
Jade D. Kharats <jade.kharats@gmail.com>
2019-03-05 11:31:49 +0100

    feat(sentry): add sentry/raven lib

--- src/romanweb.cr
+++ src/romanweb.cr
@@ -1,12 +1,23 @@
 require "roman"
 require "kemal"
+# require "raven"
+
+# Raven.configure do |config|
+#   config.dsn = ENV["SENTRY_URL"]
+# end
 
 get "/" do
   "Hello World!"
 end
 
+# get "/errors" do
+#   1 / 0
+# end
+
 get "/roman/:number" do |env|
+  # Raven.capture do
   Roman.convert(env.params.url["number"].to_i)
+  # end
 end
 
 
